86d6ae0edf194a219cc6062bc4d85dc0ac1aa955
longkzt12001@gmail.com <0868338314L0ng>
2024-02-22 21:12:25 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ItemList from './component/listItem'
+import Container from './container/home'
+
+export default function App() {
+  return <div className="w-full md:flex justify-center bg-[#FBFBFB]">
+
+
+    {/* <Button text='Click Me'/> */}
+    <Container />
+    {/* <ItemList /> */}
+  </div>
+}
---
+++ src/container/home/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../component/header";
+import ListItem from "../../component/listItem";
+
+export default function Layout() {
+    return (
+        <div className="w-full bg-[#FBFBFB]">
+            <Header></Header>
+            <ListItem></ListItem>
+        </div>
+    )
+}
